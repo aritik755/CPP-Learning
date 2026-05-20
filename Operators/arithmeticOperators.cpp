59dceb8646535e4f6e124adb5f2a7519163f6535
aritik755 <aritik755@gamil.com>
2026-05-20 17:38:47 +0530

    CHAPTER 2

---
+++ Operators/arithmeticOperators.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int num1 = 24;
+  int num2 = 12;
+
+  cout<<num1+num2<<endl; // Addition 
+  cout<<num1-num2<<endl; // Substraction
+  cout<<num1*num2<<endl; // Multiplication
+  cout<<num1/num2<<endl; // Division 
+  cout<<num1%num2<<endl; // Modulous/Remainder
+  return 0;
+} 
