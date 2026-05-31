825e955c6fd6dda4e5f038e58f954f03277a685d
aritik755 <aritik755@gamil.com>
2026-05-31 11:34:32 +0530

    Ques4

---
+++ Time and Space Complexity/ques4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+  
+  int n = 5;
+
+  for(int i = 0; i < n; i++){ // In this loop its running 'n' times.
+    for(int j = 0; j < n; j++){ // Similarly in this loop its running 'n' times.
+      cout<<"*"<<" ";
+    }
+    cout<<endl;
+  }
+
+  // So the total loop is running nxn = n^2 times.
+  // The time complexity of this code is O(n^2).
+
+  return 0;
+}
