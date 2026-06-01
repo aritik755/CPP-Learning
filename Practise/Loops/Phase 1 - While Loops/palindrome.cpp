bae91a8a10c2a7d11c6f33b06e1b276643ae5c0c
aritik755 <aritik755@gamil.com>
2026-06-01 18:55:02 +0530

    Problem-13

---
+++ Practise/Loops/Phase 1 - While Loops/palindrome.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int digit;
+  cout<<"Enter a number: ";
+  cin>>digit;
+
+  int originalDigit = digit;
+
+  int reversedDigit = 0;
+  while(digit > 0){
+    int lastDigit = digit % 10;
+    reversedDigit = reversedDigit*10 + lastDigit;
+    digit /= 10;
+  }
+  cout<<reversedDigit<<endl;
+  
+  if(originalDigit == reversedDigit){
+    cout<<"The given number is palindrome."<<endl;
+  }
+  else{
+    cout<<"The given number is not palindrome."<<endl;
+  }
+
+  return 0;
+}
