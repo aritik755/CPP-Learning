e08cee99723bc2633d253405799eaeb50a2db88b
aritik755 <aritik755@gamil.com>
2026-06-01 17:57:17 +0530

    Problem-4

---
+++ Practise/Loops/Phase 1 - While Loops/printOddNumbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int n;
+  cout<<"Enter the value of n: ";
+  cin>>n;
+
+  int i = 1;
+  cout<<"Even Numbers between the given input: ";
+  while(i <= n){
+    if(i % 2 != 0){
+      cout<<i<<"\n";
+    }
+  i++;
+  }
+  cout<<endl;
+
+  return 0;
+}
