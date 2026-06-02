40987786e2d14fecf469219aa93755e51a3fddf9
aritik755 <aritik755@gmail.com>
2026-06-02 19:56:23 +0530

    Problem-19

---
+++ Practise/Loops/Phase 1 - While Loops/printFibonacciSeries.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int n;
+  cout<<"Enter the number of terms in the series: ";
+  cin>>n;
+
+  int i = 0;
+  int a = 0;
+  int b = 1;
+  cout<<"Fibonacci Series upto given terms: ";
+  cout<<a<<" "<<b<<" ";
+  while(i < n){
+    
+    int next = a + b;
+    a = b;
+    b = next;
+    
+    cout<<next<<" ";
+
+    i++;
+  } 
+
+  return 0;
+}
