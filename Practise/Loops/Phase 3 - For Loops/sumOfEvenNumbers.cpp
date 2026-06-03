fb1a4229396bd745fd86a4d503bcb53a4261a501
aritik755 <aritik755@gmail.com>
2026-06-03 22:45:55 +0530

    Problem-20

---
+++ Practise/Loops/Phase 3 - For Loops/sumOfEvenNumbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int n;
+  cout<<"Enter the number of terms: ";
+  cin>>n;
+
+  int sum = 0;
+  for(int i = 1; i <= n; i++){
+    if(i % 2 == 0){
+      sum += i;
+    }
+  }
+  cout<<"Sum of even numbers upto given input: "<<sum<<endl;
+
+  return 0;
+}
