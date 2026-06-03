ca15f8f5b50cc28c7078557704821938a8f72c04
aritik755 <aritik755@gmail.com>
2026-06-03 22:40:07 +0530

    Problem-19

---
+++ Practise/Loops/Phase 3 - For Loops/sumOfFirstnNumbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int n;
+  cout<<"Enter the number of terms: ";
+  cin>>n;
+
+  int sum = 0;
+  for(int i = 1; i <= n; i++){
+    sum += i;
+  }
+  cout<<"Sum of terms from 1 to given input: "<<sum<<endl;
+
+  return 0;
+}
