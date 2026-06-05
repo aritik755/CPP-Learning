2c4dc3d910cd854b1290523455e663e55250cf2f
aritik755 <aritik755@gmail.com>
2026-06-05 12:00:10 +0530

    Problem-3

---
+++ Practise/Loops/Phase 8 - Star Pattern Printing/printnStarsInaSingleRow.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int n;
+  cout<<"Enter the number of stars: ";
+  cin>>n;
+
+  for(int i = 0; i < n; i++){
+    cout<<"*"<<" ";
+  }
+
+  return 0;
+}
