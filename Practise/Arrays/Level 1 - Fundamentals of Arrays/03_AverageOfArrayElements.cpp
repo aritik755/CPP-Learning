b9d5aa4f36b11b7a55037e2cde1568a2cdac5d09
aritik755 <aritik755@gmail.com>
2026-06-06 14:18:37 +0530

    Problem-3

---
+++ Practise/Arrays/Level 1 - Fundamentals of Arrays/03_AverageOfArrayElements.cpp
@@ -0,0 +1,27 @@
+// 1. Input n and take n integers into an array; print them.
+#include<iostream>
+using namespace std;
+int main(){
+
+  int n;
+  cout<<"Enter the size of array: ";
+  cin>>n;
+
+  int array[n];
+
+  cout<<"Enter the elements of array: "<<endl;
+  for(int i = 0; i < n; i++){
+    cin>>array[i];
+  }
+
+  int sum = 0;
+  for(int i = 0; i < n; i++){
+    sum += array[i];
+  }
+
+  int average = sum / n;
+
+  cout<<"Average of Elements of array: "<<average<<endl;
+
+  return 0;
+}
