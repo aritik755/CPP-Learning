44defad3c8a963bd233cbf1ec9041035adb8a0d4
aritik755 <aritik755@gmail.com>
2026-06-08 12:31:03 +0530

    Problem-9

---
+++ Practise/Arrays/Level 4 - Aggregate & Comparative Thinking/09_FrequencyOfArraysCountEachNumber.cpp
@@ -0,0 +1,34 @@
+// 9. Create a frequency array of numbers (count occurrence of each number).
+#include<iostream>
+using namespace std;
+int main(){
+
+  int array1[] = {1,2,3,1,1,2,3};
+  int n = 7;
+
+  int maxElement = 0;
+  for(int i = 0; i < n; i++){
+    if(array1[i] > maxElement){
+      maxElement = array1[i];
+    }
+  }
+
+  int freq[maxElement + 1];
+
+  for(int i = 0; i <= maxElement; i++){
+    freq[i] = 0;
+  }
+
+  for(int i = 0; i < n; i++){
+    freq[array1[i]]++;
+  }
+
+  cout << "Frequencies:" << endl;
+  for(int i = 0; i <= maxElement; i++){
+      if(freq[i] > 0){
+        cout << i << " occurs " << freq[i] << " times" << endl;
+      }
+    }
+
+  return 0;
+}
