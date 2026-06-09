b6a0c1b31e3e337f114b362c951bb30b4e2114d9
aritik755 <aritik755@gmail.com>
2026-06-09 22:59:30 +0530

    Call By Reference Using Pointers

---
+++ Pointers/Pointer-2/callByReferenceUsingPointers.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void swap(int *x, int *y){
+  int temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
+int main(){
+
+  int x = 10;
+  int y = 20;
+
+  int *ptr1 = &x;
+  int *ptr2 = &y;
+
+  swap(ptr1, ptr2);
+
+  cout<<x<<" "<<y;
+
+  return 0;
+}
