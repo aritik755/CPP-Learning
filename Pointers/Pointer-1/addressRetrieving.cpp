36201fa0230fd27552c050b94d1c1f1ede6f84e8
aritik755 <aritik755@gmail.com>
2026-06-09 16:48:18 +0530

    Address Retrieving.

---
+++ Pointers/Pointer-1/addressRetrieving.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int x = 20;
+  int y = 21;
+  cout<< &x << " " << &y; // &x and &y is used to retrive the addresses of x and y.
+
+  return 0;
+}
