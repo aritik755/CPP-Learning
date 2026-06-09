4b2e038b4907cabefea46c4d3812f3e4fdd4759e
aritik755 <aritik755@gmail.com>
2026-06-09 16:52:08 +0530

    Basic Program

---
+++ Pointers/Pointer-1/basicProgram.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int x, y;
+  cout<<"Enter the value of x and y: ";
+  cin>>x>>y;
+
+  int *ptrx = &x;
+  int *ptry = &y;
+
+  int result;
+
+  int *ptrResult = &result;
+
+  *ptrResult = *ptrx + *ptry;
+
+  cout<<result<<" "<<*ptrResult<<endl;
+  
+  return 0;
+}
