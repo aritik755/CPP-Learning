d1c0aa54061601b6f3df8fdbccf64ee7431c2a59
aritik755 <aritik755@gmail.com>
2026-06-10 15:41:45 +0530

    Post and Pre Airthmetic Pointers

---
+++ Pointers/Pointer-2/postAndPreAirthmeticPointers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int arr[2] = {1,20};
+  int *ptr = &arr[0];
+
+  cout<<ptr<<" "<<*ptr<<endl;
+
+  cout<<*ptr++<<endl;
+
+  cout<<arr[0]<<" "<<arr[1]<<endl;
+  cout<<ptr<<" "<<*ptr<<endl;
+
+  return 0;
+}
