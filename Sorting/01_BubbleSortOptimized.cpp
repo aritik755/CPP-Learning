53ea0138dc424d3b2699c78b532c9f71a28cbead
aritik755 <aritik755@gmail.com>
2026-06-19 22:26:56 +0530

    Bubble Sort Optimized

---
+++ Sorting/01_BubbleSortOptimized.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void bubbleSort(vector<int> &v){
+  int n = v.size();
+
+  for(int i = 0; i < n-1; i++){
+    bool flag = false;
+    for(int j = 0; j < n-1-i; j++){
+      if(v[j] > v[j+1]){
+        flag = true;
+        swap(v[j],v[j+1]);
+      }
+    }
+    if(!flag) break;
+  }
+  return;
+}
+int main(){
+
+  int n;
+  cout<<"Enter the number of elements of the array: ";
+  cin>>n;
+
+  vector<int> v(n);
+  
+  cout<<"Enter the elements of array:"<<endl;
+  for(int i = 0; i < n; i++){
+    cin>>v[i];
+  }
+
+  bubbleSort(v);
+
+  cout<<"Sorted Array: "<<endl;
+  for(int i = 0; i < n; i++){
+    cout<<v[i]<<" ";
+  }
+  cout<<endl;
+
+  return 0;
+}
