137f1de63572f5e2b8a9431a7abb7af7e7cd103f
aritik755 <aritik755@gmail.com>
2026-07-05 22:21:54 +0530

    Various Functions of String

---
+++ Strings/03_FunctionsOfString.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+int main(){
+
+  // string str = "Hello";
+  // reverse(str.begin(), str.end()); // Used to reverse a string
+  // cout<<str<<endl;
+
+  // string str = "survive";
+  // cout<<str.substr(0,3)<<endl; // To fing a substring => substring(position, length)
+
+  // string s1 = "Ritik";
+  // string s2 = "Agarwal";
+
+  // cout<<(s1+" "+s2)<<endl; // Adding both the strings s1 and s2.
+
+  // char s1[20] = "Ritik";
+  // char s2[20] = "Agarwal";
+
+  // strcat(s1,s2); // Use to concatinate char arrays 
+  // cout<<s1<<endl;
+
+  // string s1 = "abcd";
+  // char ch = 'e';
+
+  // s1.push_back(ch); // Used add char in string.
+
+  // cout<<s1<<endl;
+
+  // string s = "Ritik";
+  // cout<<s.size()<<endl; // s.size() is used to find the length of a string.
+
+  // char ch[20] ="abcde";
+  // cout<<strlen(ch)<<endl; // To find the length of the char array.
+
+  // int num = 432;
+  // string str = to_string(num); // Convert a number into string.
+
+  // str += "1";
+  // cout<<str<<endl;
+
+  return 0;
+}
