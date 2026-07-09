89276c07c0966cc017b4cc977cfa7df8c2eb4733
aritik755 <aritik755@gmail.com>
2026-07-09 21:46:52 +0530

    Diamond Problem

---
+++ Object Oriented Programming/08_DiamondProblem.cpp
@@ -0,0 +1,39 @@
+// In, Diamond Problem base class has multiple parent class having a common ancestor.
+
+#include<iostream>
+using namespace std;
+
+class Parent{
+  public:
+    Parent(){
+      cout<<"Parent class"<<endl;
+    }
+};
+
+class Child1: public Parent{
+  public:
+    Child1(){
+      cout<<"Child1 class"<<endl;
+    }
+};
+
+class Child2: public Parent{
+  public:
+    Child2(){
+      cout<<"Child2 class"<<endl;
+    }
+};
+
+class GrandChild: public Child1, public Child2{
+  public:
+    GrandChild(){
+      cout<<"GrandChild class"<<endl;
+    }
+};
+
+int main(){
+
+  GrandChild c;
+
+  return 0;
+}
