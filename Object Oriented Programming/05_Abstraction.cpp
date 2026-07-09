6ffa67897e20978302d88dd3a394bfa567e4a98c
aritik755 <aritik755@gmail.com>
2026-07-09 21:43:09 +0530

    Abstraction in OOPS

---
+++ Object Oriented Programming/05_Abstraction.cpp
@@ -0,0 +1,12 @@
+// Abstraction enables us to display essential information while hiding implementation details.
+// For Eg: pow(x,y) = x^y
+// In this example implementation of program is hidden and essential details are displayed.
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+
+  cout<<pow(3,4)<<endl;
+
+  return 0;
+}
