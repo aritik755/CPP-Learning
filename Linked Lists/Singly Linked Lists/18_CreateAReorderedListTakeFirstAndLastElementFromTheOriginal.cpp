11b65c9ae748d2b5cac9efef4757b98aeda5528e
aritik755 <aritik755@gmail.com>
2026-07-31 14:34:52 +0530

    Creating a list by taking first and last alternatively

---
+++ Linked Lists/Singly Linked Lists/18_CreateAReorderedListTakeFirstAndLastElementFromTheOriginal.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+  public:
+  int val;
+  Node* next;
+
+  Node(int data){
+    val = data;
+    next = NULL;
+  }
+};
+
+class LinkedList{
+  public:
+    Node* head;
+
+    LinkedList(){
+      head=NULL;
+    }
+
+    void insertAtTail(int value){
+
+      Node* new_node = new Node(value);
+
+      if(head == NULL){
+        head = new_node;
+        return;
+      }
+
+      Node* temp = head;
+      while(temp->next != NULL){
+        temp = temp->next;
+      }
+      temp->next = new_node;
+    }
+
+      void display(){
+      Node* temp = head;
+      while(temp != NULL){
+        cout<<temp->val<<"->";
+        temp = temp->next;
+      }cout<<"NULL"<<endl;
+  }
+};
+
+Node* reorderedLL(Node* &head){
+
+  // 1. Finding the middle element of the ll
+  Node* slow = head;
+  Node* fast = head;
+  while(fast && fast->next){
+    slow = slow->next;
+    fast = fast->next->next;
+  }
+
+  // now slow is pointing to the middle element
+  // 2. seperate the ll mid and reverse second half
+  Node* curr = slow->next;
+  slow->next = NULL;
+  Node* prev = slow;
+  while(curr){
+    Node* nextPtr = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = nextPtr;
+  }
+
+  // 3. merging the two halfs of the ll 
+  Node* ptr1 = head;
+  Node* ptr2 = prev;
+  while(ptr1 != ptr2){
+    Node* temp = ptr1->next;
+    ptr1->next = ptr2;
+    ptr1 = ptr2;
+    ptr2 = temp;
+  }
+  return head;
+}
+
+int main(){
+
+  LinkedList ll1;
+  ll1.insertAtTail(1);
+  ll1.insertAtTail(2);
+  ll1.insertAtTail(3);
+  ll1.insertAtTail(4);
+  ll1.insertAtTail(5);
+  ll1.insertAtTail(6);
+  ll1.insertAtTail(7);
+  ll1.display();
+  ll1.head = reorderedLL(ll1.head);
+  ll1.display();
+  
+  return 0;
+}
