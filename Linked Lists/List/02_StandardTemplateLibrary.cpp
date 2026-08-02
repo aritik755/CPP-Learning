b3c78c7e0310252077401350538e50988d10763a
aritik755 <aritik755@gmail.com>
2026-08-02 21:14:59 +0530

    Standard Template Library(STL)

---
+++ Linked Lists/List/02_StandardTemplateLibrary.cpp
@@ -0,0 +1,24 @@
+// STL(Standrd Template Library):-
+// Set of template classes for implementing commonly used data structures & functions.
+// For eg: vector<int>, vector<char>
+
+// 3 major components of STL:-
+// a. Containers
+// b. Iterators
+// c. Algorithms
+
+// Containers:- Used to hold data of same type.
+// For eg:-
+// i. vector:- Classes that defines the dynamic array.
+// ii. list:- Class for doubly linked list.
+// iii. map:- Used to store unqiue key value pairs.
+// iv. set:- Used to store unique values.
+
+// Iterators:- These are pointer like entities that are used to iterate/traverse the container.
+// For eg:- Defining a vector:- vector<int> v = {1,2,3,4,5};
+//          Defining a iterator to traverse this vector(container): vector<int> :: iterator itr;
+//          itr.begin() = 1 (First Element of the container will be fetched using this function)
+
+// Algorithms:- Functions that are used to manipulate data in the containers.
+// For eg:- sort(v.begin(), v.end()), min(a,b), max(a,b)
+
