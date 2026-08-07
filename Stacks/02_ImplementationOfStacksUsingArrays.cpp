29065ac2d708c02574dc1b49235a8e2c97d9909c
aritik755 <aritik755@gmail.com>
2026-08-07 22:18:06 +0530

    Implementation of Stacks using Arrays

---
+++ Stacks/02_ImplementationOfStacksUsingArrays.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+class Stack{
+  int capacity;
+  int* arr;
+  int top;
+
+  public:
+    Stack(int c){
+      this->capacity = c;
+      arr = new int[c];
+      this->top = -1; 
+    }
+    
+  void push(int data){
+    if(this->top == this->capacity - 1){
+      cout<<"Overflow"<<endl;
+      return;
+    }
+    this->top++;
+    this->arr[this->top] = data;
+  }
+
+  int pop(){
+    if(this->top == -1){
+      cout<<"Underflow"<<endl;
+      return INT16_MIN;
+    }
+    this->top--; 
+  }
+
+  int getTop(){
+    if(this->top == -1){
+      cout<<"Underflow"<<endl;
+      return INT16_MIN;
+    }
+    return this->arr[this->top];
+  }
+
+  bool isFull(){
+    return this->top == this->capacity - 1;
+  }
+
+  bool isEmpty(){
+    return this->top == -1;
+  }
+
+  bool size(){
+    return this->top + 1;
+  }
+};
+
+int main(){
+
+  Stack st(5);
+  st.push(1);
+  st.push(2);
+  st.push(3);
+  cout<<st.getTop()<<endl;
+  st.push(4);
+  st.push(5);
+  cout<<st.getTop()<<endl;
+  st.push(6);
+
+  st.pop();
+  st.pop();
+  cout<<st.getTop()<<endl;
+
+  return 0;
+}
+
