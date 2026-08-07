2e7396cd34db3aa12be77c2142670d2fc99c5c60
aritik755 <aritik755@gmail.com>
2026-08-07 22:17:30 +0530

    Introduction to Stacks

---
+++ Stacks/01_Stacks.cpp
@@ -0,0 +1,14 @@
+// Stacks are data structures that follows LIFO(Last In First Out) Principle.
+// In stacks elements can be added and remvoed only at the top nor in between neither in end.
+
+// Functions of the stack:-
+
+// Push :- For inserting a element in the stack(on the top).
+// Pop :- For removing a element from the stack(from the top).
+// isEmpty :- To check if the stack is empty or not.
+// isFull :- To check whether the stack is full or not.
+// size :- For getting the size of the stack.
+// Top :- To check the top most element of the stack.
+
+// Overflow :- Adding elements after exceding the size of of stack is defined as Overflow.
+// Underflow :- Removing the elements from the empty stack is called Underflow.
