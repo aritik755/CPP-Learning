8e504205308c67f538631aa45d01bad7d50f6f1a
aritik755 <aritik755@gmail.com>
2026-08-07 22:20:39 +0530

    Insert a Element at Bottom of Stack

---
+++ Stacks/06_InsertAElementAtBottomOfStack.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void insertAtBottom(stack<int> &st, int x){
+  stack<int > temp;
+  while(!st.empty()){
+    int curr = st.top();
+    st.pop();
+    temp.push(curr);
+  }
+  st.push(x);
+  while(!temp.empty()){
+    int curr = temp.top();
+    temp.pop();
+    st.push(curr);
+  }
+}
+
+void f(stack<int> &st, int x){
+  if(st.empty()){
+    st.push(x);
+    return;
+  }
+  int curr = st.top();
+  st.pop();
+  f(st, x);
+  st.push(curr);
+}
+
+int main(){
+  stack<int> st;
+  st.push(1);
+  st.push(2);
+  st.push(3);
+  st.push(4);
+  st.push(5);
+  insertAtBottom(st , 0);
+
+  while(!st.empty()){
+    int curr = st.top();
+    st.pop();
+    cout<<curr<<endl;
+  }
+
+  return 0;
+}
