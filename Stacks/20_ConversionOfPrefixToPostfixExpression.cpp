bcd50742d99e7849d3a3e2d6ee8ee85e12ee0bf0
aritik755 <aritik755@gmail.com>
2026-08-22 12:35:52 +0530

    Conversion of Prefix to Postfix Expression

---
+++ Stacks/20_ConversionOfPrefixToPostfixExpression.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cmath>
+#include<cstring>
+#include<algorithm>
+#include<stack>
+using namespace std;
+
+string eval(string &pre){
+  stack<string> st;
+  reverse(pre.begin(), pre.end());
+  for(int i = 0; i < pre.size(); i++){
+    if(isdigit(pre[i])){
+      st.push(to_string(pre[i] - '0'));
+    }
+    else{
+      string v1 = st.top();
+      st.pop();
+      string v2 = st.top();
+      st.pop();
+      string newxp = v1 + v2 + pre[i];
+      st.push(newxp);
+    }
+  }
+  return st.top();
+}
+
+int main(){
+
+  string s = "*+31-15";
+  cout<<eval(s)<<endl;
+
+  return 0;
+}
