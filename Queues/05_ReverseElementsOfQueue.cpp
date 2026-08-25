24daac69f7d1fa5d4f17f36e7301c0d00b6ffbd7
aritik755 <aritik755@gmail.com>
2026-08-25 22:50:11 +0530

    Reversing Elements of Queues

---
+++ Queues/05_ReverseElementsOfQueue.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<stack>
+#include <queue>
+using namespace std;
+int main(){
+  queue<int> input;
+  input.push(10);
+  input.push(20);
+  input.push(30);
+  input.push(40);
+  input.push(50);
+
+  stack<int> st;
+  
+  while(!input.empty()){
+    st.push(input.front());
+    input.pop();
+  }
+  
+  while(!st.empty()){
+    input.push(st.top());
+    st.pop();
+  }
+
+  while(!input.empty()){
+    cout<<input.front()<<" ";
+    input.pop();
+  }
+
+
+  return 0;
+}
