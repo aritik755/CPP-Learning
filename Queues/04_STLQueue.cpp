1cebd4ca485ad691df336714f6ca52bc4f16da60
aritik755 <aritik755@gmail.com>
2026-08-25 22:49:48 +0530

    STL Function of Queues

---
+++ Queues/04_STLQueue.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main(){
+
+  queue<int> qu;
+  qu.push(10); // Enqueue
+  qu.push(20);
+  qu.push(30);
+  qu.pop(); // Dequeue
+  qu.push(40);
+
+  while (!qu.empty())
+  {
+    cout << qu.front() << " ";
+    qu.pop();
+  }
+  return 0;
+}
